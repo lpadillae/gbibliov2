ba7ca285133667da6e9f7b98dbfb29dd2101ed06
Lennin Padilla <lpadilla@nowoptics.com>
2026-03-08 14:50:50 -0600

    Fixing book cover issue

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,19 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
-        pathname: '/**', // This allows any path under the hostname
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'books.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'openlibrary.org',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
